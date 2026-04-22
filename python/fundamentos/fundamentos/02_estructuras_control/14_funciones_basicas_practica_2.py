bfb16c4117154937fbc4cfe248c7dfdea3daea97
sebast1an-bust4mante <sebastianbustamante@liceovvh.cl>
2026-04-22 15:15:47 -0400

    miercoles22

--- python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica_2.py
+++ python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica_2.py
@@ -1,34 +1,51 @@
+import os
 #Ejercicio 1-----------------------------------
 #
 # Calcula experiencia
 def multiplica_por_2():
-  for i in range(0, 2):
-    if(i % 2 == 0):
-     return[i]
-
-resultado = multiplica_por_2(5)
-print(resultado)
+  for i in range(0, 12, 2):
+    if(i )
+  def ejercicio1():
+   resultado1 = multiplica_por_2(5)
+  print(resultado1)
 # Debe retornar: [0, 2, 4, 6, 8, 10]
 
 #Ejercicio 2-----------------------------------
 # Analiza publicaciones
-def suma_y_resta():
-    pass
-suma_y_resta([120, 115])
+def suma_y_resta(list):
+    suma = list[0] + list[1]
+    resta = list[0] - list[1]
+    print(f"suma : {suma}")
+    return resta
+def ejercicio2():
+  resta = suma_y_resta([120, 115])
+  print(f"retorno / resta; {resta}")
 # Imprime: 235 y retorna: 5
 
 #Ejercicio 3-----------------------------------
 # Puntaje ajustado
-
-sumatoria_menos_longitud([10, 5, 3, 7])
+def sumatoria_menos_longitud(sumatoria):
+    total = sum(sumatoria)
+    longitud = len(sumatoria)
+    resultado = total - longitud
+    return resultado
+def ejercicio3():
+  retornar = sumatoria_menos_longitud([10, 5, 3, 7])
+  print(f"El resultado del retorno es: {retornar}")
 # Suma total = 25, longitud = 4, debe retornar: 21
 
 #Ejercicio 4-----------------------------------
 # Ajusta visualizaciones
-valores_multiplicados_segundo([100, 3, 50, 20])
+def valores_multiplicados_segundo():
+  valores_multiplicados_segundo([100, 3, 50, 20])
 # Imprime: 4 y retorna: [300, 9, 150, 60]
 
 #Ejercicio 5-----------------------------------
-valores_multiplicados_segundo([100])
+def valores_multiplicados_segundo():
+
+
+  valores_multiplicados_segundo([100])
 # Imprime: 1 y retorna: []
 
+def limpiar_consola():
+  os.system('')
