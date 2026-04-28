10ffc344e849e577eaa0cbfcfcfe570a828c7bfa
sebast1an-bust4mante <sebastianbustamante@liceovvh.cl>
2026-04-28 12:59:05 -0400

    martes28(1)

--- python/fundamentos/oop/usuario_streaming.py
+++ python/fundamentos/oop/usuario_streaming.py
@@ -7,19 +7,38 @@ class UsuarioStreaming:
 
 def agregar_a_lista(self, titulo):
        """Agrega un contenido a la lista de reproducción del usuario."""
-       pass
+       self.lista_reproduccion.append(titulo)
+       print(f"titulo '{titulo}' agregado correctamente.") 
 
 
 def ver_contenido(self, titulo):
        """Simula que el usuario reproduce un contenido."""
-       pass
+       if titulo in self.lista_reproduccion:
+             print(f"El usuario {self.nombre} esta viendo '{titulo}'")
+       else:
+           print(f"Titulo no encontrado {titulo}")
 
 
 def cambiar_suscripcion(self, nueva_suscripcion):
        """Cambia el tipo de suscripción del usuario."""
-       pass
+       susAntigua = self.suscripcion
+       self.suscripcion = nueva_suscripcion
+       print(f"Suscripción cambió de {susAntigua} a {nueva_suscripcion}")
 
 
 def mostrar_info_usuario(self):
        """Muestra la información del usuario y su lista de reproducción."""
-       pass
+       print(f"Nombre: {self.nombre}")
+       print(f"Email: {self.email}")
+       print(f"Suscripcion: {self.suscripcion}")
+       if len(self.lista_reproduccion) == 0:
+           print("La lista de reproduccion está vacia.")
+       else:
+            print(f"Lista de reproduccion: \n- {"\n- ".join(self.lista_reproduccion)}")
+
+#Instancias 
+camilo = UsuarioStreaming("camilo")
+camilo.agregar_a_lista("La casa de papel")
+camilo.cambiar_suscripcion
+camilo.ver_contenido
+camilo.mostrar_info_usuario()
